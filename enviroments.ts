1e722a3687e33899688fd3b42c1692d337fa97b6
sofiavalk <esofiavalcarcel73@gmail.com>
2026-02-06 14:20:08 -0500

    Conexion base de datos con MySql

---
+++ enviroments.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+
+export const enviroments = {
+    dev: '.env',
+    stg: '.stg.env',
+    prod: '.prod.env',
+};
---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
